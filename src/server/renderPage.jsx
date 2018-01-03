f6af923d270fb6cebc11cd0695ceda4f4a0aa971
Markus <markus.s.englund@gmail.com>
2018-01-03 20:18:38 +0100

    get css to work

--- src/server/renderPage.jsx
+++ src/server/renderPage.jsx
@@ -29,8 +29,10 @@ export default function renderPage(req: $Request, res: $Response) {
     <!DOCTYPE html>
     <html>
       <head>
-        <meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/public/bundle.css">
         ${helmet.meta.toString()}
         ${helmet.title.toString()}
       </head>
